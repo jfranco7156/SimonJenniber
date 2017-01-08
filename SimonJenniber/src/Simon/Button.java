789de31b956ecbe8b709028c877559b15aee67f3
Riaked <Riaked@DESKTOP-UVEVF3A.home>
2017-01-08 15:57:55 -0500

    classes

---
+++ SimonJenniber/src/Simon/Button.java
@@ -0,0 +1,49 @@
+package Simon;
+
+import java.awt.Color;
+
+public class Button extends Component implements ButtonInterfaceJenniber {
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ButtonInterfaceJenniber getAButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setX(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setY(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dim() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAction(Action action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ SimonJenniber/src/Simon/Progress.java
@@ -0,0 +1,23 @@
+package Simon;
+
+public class Progress extends Component implements ProgressInterfaceJenniber {
+
+	@Override
+	public void gameOver() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRound(int r) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSequenceSize(int s) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
